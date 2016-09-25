7b577cabdc227df4b303bb3680c039b270e720ed
Jones Melton <shavah@gmail.com>
2016-09-25 13:46:32 -0500

    Correct dotenv loading in test

--- test/api_keys.js
+++ test/api_keys.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config
+require('dotenv').config();
 
 let chai = require('chai');
 const expect = chai.expect;
@@ -7,9 +7,9 @@ const expect = chai.expect;
 
 describe("API keys", () => {
   it("has the keys loaded", () => {
-    expect(process.env.CONSUMER_KEY).to.be.ok
-    expect(process.env.CONSUMER_SECRET).to.be.ok
-    expect(process.env.ACCESS_TOKEN).to.be.ok
-    expect(process.env.TOKEN_SECRET).to.be.ok
+    expect(process.env.CONSUMER_KEY).to.exist
+    expect(process.env.CONSUMER_SECRET).to.exist
+    expect(process.env.ACCESS_TOKEN).to.exist
+    expect(process.env.TOKEN_SECRET).to.exist
   })
 })
