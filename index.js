1ac98603dfadcdba3eae1586a98d847856e6fead
Jones Melton <shavah@gmail.com>
2016-09-26 13:38:16 -0500

    Rename and refactor functions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
+// the wrapper around the twitter interface I'm using
 const Twit = require('twit');
 
+// initialization of the wrapper with api keys
 const T = new Twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -9,7 +11,7 @@ const T = new Twit({
   access_token_secret: process.env.TOKEN_SECRET,
 });
 
-function doTweet(content) {
+function tweet(content) {
   T.post('statuses/update', { status: content }, (err, data, response) => {
     // just printing the results here but you could set it a variable or return it
     console.log(data)
@@ -28,25 +30,52 @@ function userTweets(username) {
   })
 }
 
-// grab the argument passed in to the program
+function myRetweets() {
+  T.get('statuses/retweets_of_me', (err, data, response) => {
+    console.log(data)
+  })
+}
+
+function following() {
+  T.get('friends/list', (err, data, response) => {
+    console.log(data)
+  })
+}
+
+function followers() {
+  T.get('followers/list', (err, data, response) => {
+    console.log(data)
+  })
+}
+
+// get the action we're to perform
 const command = process.argv[2]
 
 // grab the rest of the user input and turn it into a string
 const input = process.argv.slice(3, process.argv.length).join(' ')
 
-
+// takes command from the user and performs the appropriate action
 switch(command) {
   case undefined: 
-    console.log("input a thing")
+    console.log("Give a command: tweet, search, user, or retweets")
     break;
   case "tweet":
-    doTweet(input)
+    tweet(input)
     break;
   case "search":
     searchTweets(input)
     break;
   case "user":
     userTweets(input)
+    break;
+  case "retweets":
+    myRetweets()
+    break;
+  case "following":
+    following()
+    break;
+  case "followers":
+    followers()
     break;
 }
 
